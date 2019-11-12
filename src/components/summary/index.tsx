2afd8f26eb9d3ccaace2a5d779a6ee305a0ac58f
Mitali Patel <mitali.khundiwala@gmail.com>
2019-11-12 18:22:45 +0400

    created tiny URL

--- src/components/summary/index.tsx
+++ src/components/summary/index.tsx
@@ -6,7 +6,11 @@ const SummaryComponent = () => {
     return (
         <div className="summary">
             <div className="info">
-                <div className="greeting"><h1>Namaste, <br /> I’m <span className="name">Mitali Patel</span></h1></div>
+                <div className="greeting">
+                    <h1>
+                        Namaste, <br /> I’m <span className="name">Mitali Patel</span>
+                    </h1>
+                </div>
                 <p className="headline">A Front-end Developer developing modern web apps with HTML technologies for more than 8 years</p>
                 <div className="social-links">
                     <a href="https://twitter.com/ilatim" target="_blank">
@@ -18,7 +22,7 @@ const SummaryComponent = () => {
                     <a href="https://github.com/mitalikhundiwala" target="_blank">
                         <i className="fa fa-github"></i>
                     </a>
-                    <a href="https://ucc832f58322abaa204d680af978.dl.dropboxusercontent.com/cd/0/get/AsO9jJCcpf53464qiAabqaJrUpbo3Y_dD_QBvF2GP6m_R2RGhfu3OvnBxGrP-8NdhsfmFSvwfd3xNtwZ7DWvEkQcGT9PeXs_vIOUdWru_72SW5LUB3r0n2xLkWnAez2t0pI/file" target="_blank">
+                    <a href="https://tinyurl.com/uq5mt8u" target="_blank">
                         <i className="fa fa-download"></i>
                     </a>
                 </div>
