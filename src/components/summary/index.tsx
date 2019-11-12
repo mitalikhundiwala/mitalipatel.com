8ca223e1ffa36b0bee8fdd320055b360e9210cf7
Mitali Patel <mitali.khundiwala@gmail.com>
2019-11-12 18:20:41 +0400

    Added resume download button

--- src/components/summary/index.tsx
+++ src/components/summary/index.tsx
@@ -18,6 +18,9 @@ const SummaryComponent = () => {
                     <a href="https://github.com/mitalikhundiwala" target="_blank">
                         <i className="fa fa-github"></i>
                     </a>
+                    <a href="https://ucc832f58322abaa204d680af978.dl.dropboxusercontent.com/cd/0/get/AsO9jJCcpf53464qiAabqaJrUpbo3Y_dD_QBvF2GP6m_R2RGhfu3OvnBxGrP-8NdhsfmFSvwfd3xNtwZ7DWvEkQcGT9PeXs_vIOUdWru_72SW5LUB3r0n2xLkWnAez2t0pI/file" target="_blank">
+                        <i className="fa fa-download"></i>
+                    </a>
                 </div>
             </div>
         </div>
